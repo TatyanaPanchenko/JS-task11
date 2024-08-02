2eb84f1eeefbface84acf78218643210d16c40bf
TatyanaPanchenko <tatyanapanchenko.web@gmail.com>
2024-08-02 19:01:04 +0300

    change

--- task11.1/authScript.js
+++ task11.1/authScript.js
@@ -1,7 +1,5 @@
 const nameUser = document.querySelector(".name");
 const idNumber = document.querySelector(".id-number");
 const idFromStorage = JSON.parse(localStorage.getItem("chooseUser"));
-console.log(idFromStorage);
 nameUser.textContent = idFromStorage.userName;
 idNumber.textContent = idFromStorage.id;
-nameUser.textContent = "dsf";
